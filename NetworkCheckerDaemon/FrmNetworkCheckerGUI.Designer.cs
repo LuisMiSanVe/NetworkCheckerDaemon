3b8d58b509c6d4d60d937a2403cee4cd588bbda6
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-05-13 10:57:25 +0200

    Add files via upload

--- NetworkCheckerDaemon/FrmNetworkCheckerGUI.Designer.cs
+++ NetworkCheckerDaemon/FrmNetworkCheckerGUI.Designer.cs
@@ -38,7 +38,26 @@
             toolStripSeparator1 = new ToolStripSeparator();
             tlstrpmnuitm_HideShow = new ToolStripMenuItem();
             tlstrpmnuitm_Close = new ToolStripMenuItem();
+            grpbx_Advanced = new GroupBox();
+            txtbx_hostValue = new TextBox();
+            lbl_FragmentValue = new Label();
+            lbl_Fragment = new Label();
+            lbl_TTLValue = new Label();
+            lbl_BufferValue = new Label();
+            lbl_TripTimeValue = new Label();
+            lbl_TTL = new Label();
+            lbl_Buffer = new Label();
+            lbl_TripTime = new Label();
+            lbl_host = new Label();
+            lbl_Status = new Label();
+            lbl_StatusValue = new Label();
+            lbl_PingStatusValue = new Label();
+            lbl_PingStatus = new Label();
+            btn_StartStop = new Button();
+            btn_Close = new Button();
+            imglst_startstop = new ImageList(components);
             cntxmnustpOptions.SuspendLayout();
+            grpbx_Advanced.SuspendLayout();
             SuspendLayout();
             // 
             // tmrCheck
@@ -92,18 +111,204 @@
             tlstrpmnuitm_Close.Text = "Close";
             tlstrpmnuitm_Close.Click += tlstrpmnuitm_Close_Click;
             // 
+            // grpbx_Advanced
+            // 
+            grpbx_Advanced.Controls.Add(txtbx_hostValue);
+            grpbx_Advanced.Controls.Add(lbl_FragmentValue);
+            grpbx_Advanced.Controls.Add(lbl_Fragment);
+            grpbx_Advanced.Controls.Add(lbl_TTLValue);
+            grpbx_Advanced.Controls.Add(lbl_BufferValue);
+            grpbx_Advanced.Controls.Add(lbl_TripTimeValue);
+            grpbx_Advanced.Controls.Add(lbl_TTL);
+            grpbx_Advanced.Controls.Add(lbl_Buffer);
+            grpbx_Advanced.Controls.Add(lbl_TripTime);
+            grpbx_Advanced.Controls.Add(lbl_host);
+            grpbx_Advanced.Location = new Point(12, 84);
+            grpbx_Advanced.Name = "grpbx_Advanced";
+            grpbx_Advanced.Size = new Size(417, 223);
+            grpbx_Advanced.TabIndex = 1;
+            grpbx_Advanced.TabStop = false;
+            grpbx_Advanced.Text = "Advanced";
+            // 
+            // txtbx_hostValue
+            // 
+            txtbx_hostValue.Enabled = false;
+            txtbx_hostValue.Location = new Point(113, 34);
+            txtbx_hostValue.Name = "txtbx_hostValue";
+            txtbx_hostValue.Size = new Size(298, 31);
+            txtbx_hostValue.TabIndex = 10;
+            txtbx_hostValue.Text = "8.8.8.8";
+            // 
+            // lbl_FragmentValue
+            // 
+            lbl_FragmentValue.AutoSize = true;
+            lbl_FragmentValue.Location = new Point(113, 187);
+            lbl_FragmentValue.Name = "lbl_FragmentValue";
+            lbl_FragmentValue.Size = new Size(96, 25);
+            lbl_FragmentValue.TabIndex = 9;
+            lbl_FragmentValue.Text = "Scanning...";
+            // 
+            // lbl_Fragment
+            // 
+            lbl_Fragment.AutoSize = true;
+            lbl_Fragment.Location = new Point(6, 187);
+            lbl_Fragment.Name = "lbl_Fragment";
+            lbl_Fragment.Size = new Size(92, 25);
+            lbl_Fragment.TabIndex = 8;
+            lbl_Fragment.Text = "Fragment:";
+            // 
+            // lbl_TTLValue
+            // 
+            lbl_TTLValue.AutoSize = true;
+            lbl_TTLValue.Location = new Point(113, 151);
+            lbl_TTLValue.Name = "lbl_TTLValue";
+            lbl_TTLValue.Size = new Size(96, 25);
+            lbl_TTLValue.TabIndex = 7;
+            lbl_TTLValue.Text = "Scanning...";
+            // 
+            // lbl_BufferValue
+            // 
+            lbl_BufferValue.AutoSize = true;
+            lbl_BufferValue.Location = new Point(113, 113);
+            lbl_BufferValue.Name = "lbl_BufferValue";
+            lbl_BufferValue.Size = new Size(96, 25);
+            lbl_BufferValue.TabIndex = 6;
+            lbl_BufferValue.Text = "Scanning...";
+            // 
+            // lbl_TripTimeValue
+            // 
+            lbl_TripTimeValue.AutoSize = true;
+            lbl_TripTimeValue.Location = new Point(113, 73);
+            lbl_TripTimeValue.Name = "lbl_TripTimeValue";
+            lbl_TripTimeValue.Size = new Size(96, 25);
+            lbl_TripTimeValue.TabIndex = 5;
+            lbl_TripTimeValue.Text = "Scanning...";
+            // 
+            // lbl_TTL
+            // 
+            lbl_TTL.AutoSize = true;
+            lbl_TTL.Location = new Point(6, 151);
+            lbl_TTL.Name = "lbl_TTL";
+            lbl_TTL.Size = new Size(42, 25);
+            lbl_TTL.TabIndex = 3;
+            lbl_TTL.Text = "TTL:";
+            // 
+            // lbl_Buffer
+            // 
+            lbl_Buffer.AutoSize = true;
+            lbl_Buffer.Location = new Point(6, 113);
+            lbl_Buffer.Name = "lbl_Buffer";
+            lbl_Buffer.Size = new Size(63, 25);
+            lbl_Buffer.TabIndex = 2;
+            lbl_Buffer.Text = "Buffer:";
+            // 
+            // lbl_TripTime
+            // 
+            lbl_TripTime.AutoSize = true;
+            lbl_TripTime.Location = new Point(6, 73);
+            lbl_TripTime.Name = "lbl_TripTime";
+            lbl_TripTime.Size = new Size(87, 25);
+            lbl_TripTime.TabIndex = 1;
+            lbl_TripTime.Text = "Trip Time:";
+            // 
+            // lbl_host
+            // 
+            lbl_host.AutoSize = true;
+            lbl_host.Location = new Point(6, 34);
+            lbl_host.Name = "lbl_host";
+            lbl_host.Size = new Size(54, 25);
+            lbl_host.TabIndex = 0;
+            lbl_host.Text = "Host:";
+            // 
+            // lbl_Status
+            // 
+            lbl_Status.AutoSize = true;
+            lbl_Status.Location = new Point(12, 9);
+            lbl_Status.Name = "lbl_Status";
+            lbl_Status.Size = new Size(136, 25);
+            lbl_Status.TabIndex = 3;
+            lbl_Status.Text = "Network Status:";
+            // 
+            // lbl_StatusValue
+            // 
+            lbl_StatusValue.AutoSize = true;
+            lbl_StatusValue.Location = new Point(154, 9);
+            lbl_StatusValue.Name = "lbl_StatusValue";
+            lbl_StatusValue.Size = new Size(96, 25);
+            lbl_StatusValue.TabIndex = 4;
+            lbl_StatusValue.Text = "Scanning...";
+            // 
+            // lbl_PingStatusValue
+            // 
+            lbl_PingStatusValue.AutoSize = true;
+            lbl_PingStatusValue.Location = new Point(154, 48);
+            lbl_PingStatusValue.Name = "lbl_PingStatusValue";
+            lbl_PingStatusValue.Size = new Size(96, 25);
+            lbl_PingStatusValue.TabIndex = 9;
+            lbl_PingStatusValue.Text = "Scanning...";
+            // 
+            // lbl_PingStatus
+            // 
+            lbl_PingStatus.AutoSize = true;
+            lbl_PingStatus.Location = new Point(12, 48);
+            lbl_PingStatus.Name = "lbl_PingStatus";
+            lbl_PingStatus.Size = new Size(133, 25);
+            lbl_PingStatus.TabIndex = 8;
+            lbl_PingStatus.Text = "Package Status:";
+            // 
+            // btn_StartStop
+            // 
+            btn_StartStop.BackgroundImage = (Image)resources.GetObject("btn_StartStop.BackgroundImage");
+            btn_StartStop.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_StartStop.Location = new Point(397, 12);
+            btn_StartStop.Name = "btn_StartStop";
+            btn_StartStop.Size = new Size(30, 30);
+            btn_StartStop.TabIndex = 10;
+            btn_StartStop.UseVisualStyleBackColor = true;
+            btn_StartStop.Click += btn_StartStop_Click;
+            // 
+            // btn_Close
+            // 
+            btn_Close.BackgroundImage = (Image)resources.GetObject("btn_Close.BackgroundImage");
+            btn_Close.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_Close.Location = new Point(397, 57);
+            btn_Close.Name = "btn_Close";
+            btn_Close.Size = new Size(30, 30);
+            btn_Close.TabIndex = 11;
+            btn_Close.UseVisualStyleBackColor = true;
+            btn_Close.Click += btn_Close_Click;
+            // 
+            // imglst_startstop
+            // 
+            imglst_startstop.ColorDepth = ColorDepth.Depth32Bit;
+            imglst_startstop.ImageStream = (ImageListStreamer)resources.GetObject("imglst_startstop.ImageStream");
+            imglst_startstop.TransparentColor = Color.Transparent;
+            imglst_startstop.Images.SetKeyName(0, "buttonStart.png");
+            imglst_startstop.Images.SetKeyName(1, "buttonStop.png");
+            // 
             // FrmNetworkCheckerGUI
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(439, 316);
+            Controls.Add(btn_Close);
+            Controls.Add(btn_StartStop);
+            Controls.Add(lbl_PingStatusValue);
+            Controls.Add(lbl_PingStatus);
+            Controls.Add(lbl_StatusValue);
+            Controls.Add(lbl_Status);
+            Controls.Add(grpbx_Advanced);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "FrmNetworkCheckerGUI";
             Text = "Network Checker GUI";
             FormClosing += FrmNetworkCheckerGUI_FormClosing;
             cntxmnustpOptions.ResumeLayout(false);
             cntxmnustpOptions.PerformLayout();
+            grpbx_Advanced.ResumeLayout(false);
+            grpbx_Advanced.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -116,5 +321,23 @@
         private ToolStripMenuItem tlstrpmnuitm_Close;
         private ToolStripTextBox tlstrptxtbx_Status;
         private ToolStripMenuItem tlstrpmnuitm_HideShow;
+        private GroupBox grpbx_Advanced;
+        private Label lbl_Status;
+        private Label lbl_StatusValue;
+        private Label lbl_host;
+        private Label lbl_TTL;
+        private Label lbl_TripTime;
+        private Label lbl_Buffer;
+        private Label lbl_TTLValue;
+        private Label lbl_BufferValue;
+        private Label lbl_TripTimeValue;
+        private Label lbl_PingStatusValue;
+        private Label lbl_PingStatus;
+        private Label lbl_FragmentValue;
+        private Label lbl_Fragment;
+        private Button btn_StartStop;
+        private Button btn_Close;
+        private ImageList imglst_startstop;
+        private TextBox txtbx_hostValue;
     }
 }
--- NetworkCheckerDaemon/FrmNetworkCheckerGUI.cs
+++ NetworkCheckerDaemon/FrmNetworkCheckerGUI.cs
@@ -5,7 +5,7 @@ namespace NetworkCheckerDaemon
     public partial class FrmNetworkCheckerGUI : Form
     {
         private bool allowClose = false;
-        private string host = "8.8.8.8";
+        private bool showErrorWindow = true;
         public FrmNetworkCheckerGUI()
         {
             InitializeComponent();
@@ -13,41 +13,83 @@ namespace NetworkCheckerDaemon
 
         private void tmrCheck_Tick(object sender, EventArgs e)
         {
-            Ping pingHost = new Ping();
-            PingReply reply = pingHost.Send(host);
-
-            string statusIcon = "";
-            switch (reply.RoundtripTime)
+            try
             {
-                case < 50:
-                    statusIcon = "[\u2714] ";
-                    tlstrptxtbx_Status.ForeColor = Color.Green;
-                    break;
-                case < 100:
-                    statusIcon = "[\u26A0] ";
-                    tlstrptxtbx_Status.ForeColor = Color.Orange;
-                    break;
-                case < 200:
-                    statusIcon = "[\u0021] ";
-                    tlstrptxtbx_Status.ForeColor = Color.Red;
-                    break;
-                case > 200:
-                    statusIcon = "[\u2716] ";
-                    tlstrptxtbx_Status.ForeColor = Color.Black;
-                    break;
-                default:
-                    statusIcon = "[Error] ";
-                    tlstrptxtbx_Status.ForeColor = Color.Black;
-                    break;
+                Ping pingHost = new Ping();
+                PingReply reply = pingHost.Send(txtbx_hostValue.Text);
+
+                // Style the response data
+                string statusIcon = "";
+                switch (reply.RoundtripTime)
+                {
+                    case < 50:
+                        statusIcon = "[\u2714] "; // Check Unicode Symbol
+                        tlstrptxtbx_Status.ForeColor = Color.Green;
+                        lbl_StatusValue.ForeColor = Color.Green;
+                        break;
+                    case < 100:
+                        statusIcon = "[\u26A0] "; // Warning Unicode Symbol
+                        tlstrptxtbx_Status.ForeColor = Color.Orange;
+                        lbl_StatusValue.ForeColor = Color.Orange;
+                        break;
+                    case < 200:
+                        statusIcon = "[\u0021] "; // Exclamation Unicode Symbol
+                        tlstrptxtbx_Status.ForeColor = Color.Red;
+                        lbl_StatusValue.ForeColor = Color.Red;
+                        break;
+                    case > 200:
+                        statusIcon = "[\u2716] "; // Cross Unicode Symbol
+                        tlstrptxtbx_Status.ForeColor = Color.Black;
+                        lbl_StatusValue.ForeColor = Color.Black;
+                        break;
+                    default:
+                        statusIcon = "[Error] ";
+                        tlstrptxtbx_Status.ForeColor = Color.Black;
+                        lbl_StatusValue.ForeColor = Color.Black;
+                        break;
+                }
+
+                string statusText = "";
+                if (reply.Status.ToString().Equals("Success"))
+                    statusText = "Accessible";
+                else
+                    statusText = "Not accessible";
+                
+                // Show the data
+                tlstrptxtbx_Status.Text = statusIcon + statusText;
+                lbl_StatusValue.Text = statusIcon + statusText;
+                lbl_PingStatusValue.Text = reply.Status.ToString();
+                lbl_TripTimeValue.Text = reply.RoundtripTime.ToString() + " ms";
+                lbl_BufferValue.Text = reply.Buffer.Length.ToString() + " bytes";
+                if (reply.Options != null)
+                {
+                    lbl_TTLValue.Text = reply.Options.Ttl.ToString() + " jumps";
+                    if (reply.Options.DontFragment)
+                        lbl_FragmentValue.Text = "The package can be fragmented";
+                    else
+                        lbl_FragmentValue.Text = "The package can't be fragmented";
+                }
+                else
+                {
+                    lbl_TTLValue.Text = "Couldn't determine router jumps";
+                    lbl_FragmentValue.Text = "Couldn't determine fragmentability";
+                }
             }
+            catch (Exception ex)
+            {
+                if (showErrorWindow)
+                {
+                    btn_StartStop_Click(sender, e);
+                    var result = MessageBox.Show("An unexpected error has occurred, is possible the configured host is not reachable or doesn't exist.\nDo you want a notification to show up and the Daemon to stop each time an error occurs?", ex.Message, MessageBoxButtons.YesNo, MessageBoxIcon.Error);
 
-            string statusText = "";
-            if (reply.Status.ToString().Equals("Success"))
-                statusText = "Accessible";
-            else
-                statusText = "Not accessible";
-
-            tlstrptxtbx_Status.Text = statusIcon + statusText;
+                    if (result == DialogResult.No)
+                        showErrorWindow = false;
+                }
+                else
+                {
+                    lbl_StatusValue.Text = "An error occurred.";
+                }
+            }
         }
 
         private void FrmNetworkCheckerGUI_FormClosing(object sender, FormClosingEventArgs e)
@@ -68,8 +110,8 @@ namespace NetworkCheckerDaemon
                 this.Hide();
                 tlstrpmnuitm_HideShow.Text = tlstrpmnuitm_HideShow.Text.Replace("Hide", "Show");
             }
-            else 
-            { 
+            else
+            {
                 this.Show();
                 tlstrpmnuitm_HideShow.Text = tlstrpmnuitm_HideShow.Text.Replace("Show", "Hide");
             }
@@ -79,6 +121,31 @@ namespace NetworkCheckerDaemon
         {
             allowClose = true;
             this.Close();
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            tlstrpmnuitm_Close_Click(sender,e);
+        }
+
+        private void btn_StartStop_Click(object sender, EventArgs e)
+        {
+            if (tmrCheck.Enabled)
+            {
+                tmrCheck.Enabled = false;
+                btn_StartStop.BackgroundImage = imglst_startstop.Images[0];
+                lbl_StatusValue.ForeColor = Color.Black;
+                lbl_StatusValue.Text = "The Daemon is stopped.";
+                txtbx_hostValue.Enabled = true;
+            }
+            else
+            {
+                tmrCheck.Enabled = true;
+                btn_StartStop.BackgroundImage = imglst_startstop.Images[1];
+                lbl_StatusValue.ForeColor = Color.Black;
+                lbl_StatusValue.Text = "Scanning...";
+                txtbx_hostValue.Enabled = false;
+            }
         }
     }
 }
